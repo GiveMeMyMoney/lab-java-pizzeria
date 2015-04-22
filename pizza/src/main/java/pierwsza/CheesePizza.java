7069584a44baa7ffa742c9cb00861bcde61c07cc
Marcin Styczen <test@github.com>
2015-04-22 23:41:44 +0200

    Wzorzec projektowy - pizzeria.

---
+++ pizza/src/main/java/pierwsza/CheesePizza.java
@@ -0,0 +1,14 @@
+package pierwsza;
+
+/**
+ * Created by Marcin on 2015-04-22.
+ */
+public class CheesePizza extends Pizza {
+    public CheesePizza() {
+        name = "Cheese Pizza";
+        dough = "Regular Crust";
+        sauce = "Marinara Pizza Sauce";
+        toppings.add("Fresh Mozzarella");
+        toppings.add("Parmesan");
+    }
+}
--- pizza/src/main/java/pierwsza/Start.java
+++ pizza/src/main/java/pierwsza/Start.java
@@ -1,11 +1,17 @@
 package pierwsza;
-
 /**
  * Created by Marcin on 2015-04-22.
  */
 public class Start {
 
     public static void main(String [] args) {
-        System.out.print("COSIK");
+        SimplePizzaFactory factory = new SimplePizzaFactory();
+        PizzaStore store = new PizzaStore(factory);
+
+        Pizza pizza = store.orderPizza("cheese");
+        System.out.println("We ordered a " + pizza.getName() + "\n");
+
+        pizza = store.orderPizza("veggie");
+        System.out.println("We ordered a " + pizza.getName() + "\n");
     }
 }
