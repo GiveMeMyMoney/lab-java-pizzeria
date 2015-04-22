24f2a0cd41add9897fdb81af3c2ff1c365bacd00
Marcin Styczen <test@github.com>
2015-04-22 19:17:10 +0200

    pierwsze

---
+++ pizza/src/main/java/pierwsza/Start.java
@@ -0,0 +1,11 @@
+package pierwsza;
+
+/**
+ * Created by Marcin on 2015-04-22.
+ */
+public class Start {
+
+    public static void main(String [] args) {
+        System.out.print("COSIK");
+    }
+}
